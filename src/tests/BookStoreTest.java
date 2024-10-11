8aedf09d5cd32f6fddc40f706abe230ebf960157
neriyel <neriyel.reyes@outlook.com>
2024-10-10 22:21:55 -0700

    Editted BookStore methods

--- src/tests/BookStoreTest.java
+++ src/tests/BookStoreTest.java
@@ -1,28 +1,30 @@
-package tests;
-
-import code.BookStore;
-
-
-public class BookStoreTest
-{
-    public static void main(String[] args) {
-        BookStore bookStore;
-        bookStore = new BookStore("BCIT library");
-
-        System.out.println("---------- Test print titles in alpha order: ----------");
-        bookStore.printTitlesInAlphaOrder();
-
-        System.out.println("---------- Testing Print book title with 'Wide': ----------");
-        bookStore.printBookTitle("Wide");
-
-
-        System.out.println("---------- Testing Decade with '2000' (Exists): ----------");
-        bookStore.printGroupByDecade(2000);
-
-        System.out.println("---------- Testing Decade '1900' (Does not exist): ----------");
-        bookStore.printGroupByDecade(1900);
-
-        System.out.println("---------- Finding the longest title: ----------");
-        bookStore.getLongest();
-    }
-}
+//package tests;
+//
+//import code.BookStore;
+//
+//import java.io.FileNotFoundException;
+//
+//public class BookStoreTest
+//{
+//    public static void main(String[] args) throws FileNotFoundException
+//    {
+//        BookStore bookStore;
+//        bookStore = new BookStore("BCIT library");
+//
+//        System.out.println("---------- Test print titles in alpha order: ----------");
+//        bookStore.printTitlesInAlphaOrder();
+//
+//        System.out.println("---------- Testing Print book title with 'Wide': ----------");
+//        bookStore.printBookTitle("Wide");
+//
+//
+//        System.out.println("---------- Testing Decade with '2000' (Exists): ----------");
+//        bookStore.printGroupByDecade(2000);
+//
+//        System.out.println("---------- Testing Decade '1900' (Does not exist): ----------");
+//        bookStore.printGroupByDecade(1900);
+//
+//        System.out.println("---------- Finding the longest title: ----------");
+//        bookStore.getLongest();
+//    }
+//}
